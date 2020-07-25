ac0f4653d861f8a1fc164c13bff5115232bfb888
manzhaimaksim <maxbbb88@gmail.com>
2020-07-25 14:48:13 +0300

    adding tests for anonymous user

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -28,6 +28,34 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path) # devise должен отправить на логин
       expect(flash[:alert]).to be # во flash должен быть прописана ошибка
     end
+
+    it 'Anonymous visitor cannot call help action' do
+      put :help, id: game_w_questions.id
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'Anonymous visitor cannot call answer action' do
+      put :answer, id: game_w_questions.id
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'Anonymous visitor cannot call take_money action' do
+      put :take_money, id: game_w_questions.id
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'Anonymous visitor cannot call create action' do
+      post :create
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
   end
 
   # группа тестов на экшены контроллера, доступных залогиненным юзерам
